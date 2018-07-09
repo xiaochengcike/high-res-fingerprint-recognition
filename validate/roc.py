3feb3888af3c6af78cab7663591114e1e92d0399
Gabriel Dahia <gdahia@gmail.com>
2018-07-09 12:14:32 -0300

    Add roc multiple plots

--- validate/roc.py
+++ validate/roc.py
@@ -5,25 +5,28 @@ import utils
 if __name__ == '__main__':
   import sys
 
-  # read comparisons file
-  pos = []
-  neg = []
-  with open(sys.argv[1], 'r') as f:
-    for line in f:
-      t, score = line.split()
-      score = float(score)
-      if int(t) == 1:
-        pos.append(score)
-      else:
-        neg.append(score)
+  for path in sys.argv[1:]:
+    # read comparisons file
+    pos = []
+    neg = []
+    with open(path, 'r') as f:
+      for line in f:
+        t, score = line.split()
+        score = float(score)
+        if int(t) == 1:
+          pos.append(score)
+        else:
+          neg.append(score)
 
-  # compute roc
-  fars, frrs = utils.roc(pos, neg)
+    # compute roc
+    fars, frrs = utils.roc(pos, neg)
 
-  # plot roc
-  plt.plot(fars, frrs)
+    # plot roc
+    plt.plot(fars, frrs, label=path)
+
+  plt.legend(loc='upper right')
   plt.xlabel('FAR')
   plt.ylabel('FRR')
-  plt.axis([0, 1, 0, 1])
+  plt.axis([0, 0.1, 0, 0.1])
   plt.grid()
   plt.show()
