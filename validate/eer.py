bfeb12c711db4ee57ceeac4f3b2947b5c13a7946
Gabriel Dahia <gdahia@gmail.com>
2018-07-10 08:28:26 -0300

    Add multiple EER computation

--- validate/eer.py
+++ validate/eer.py
@@ -4,15 +4,16 @@ if __name__ == '__main__':
   import sys
 
   # read comparisons file
-  pos = []
-  neg = []
-  with open(sys.argv[1], 'r') as f:
-    for line in f:
-      t, score = line.split()
-      score = float(score)
-      if int(t) == 1:
-        pos.append(score)
-      else:
-        neg.append(score)
+  for path in sys.argv[1:]:
+    pos = []
+    neg = []
+    with open(path, 'r') as f:
+      for line in f:
+        t, score = line.split()
+        score = float(score)
+        if int(t) == 1:
+          pos.append(score)
+        else:
+          neg.append(score)
 
-  print(utils.eer(pos, neg))
+    print(path, utils.eer(pos, neg))
