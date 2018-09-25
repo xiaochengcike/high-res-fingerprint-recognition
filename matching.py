46cdcd161e46be0b2c9b27df672bf2bdc34f88d4
Gabriel Dahia <gdahia@gmail.com>
2018-09-25 08:08:24 -0300

    Add item of #28, matching docstrings

--- matching.py
+++ matching.py
@@ -5,6 +5,26 @@ import utils
 
 
 def basic(descs1, descs2, pts1=None, pts2=None, thr=None):
+  '''
+  Finds bidirectional correspondences between descriptors
+  descs1 and descs2. If thr is provided, discards correspondences
+  that fail a distance ratio check with threshold thr; in this
+  case, returns correspondences satisfying SIFT's criterion.
+
+  Args:
+    descs1: [N, M] array of N descriptors of dimension M each.
+    descs2: [N, M] array of N descriptors of dimension M each.
+    pts1: sentinel argument for matching function signature
+      standardization.
+    pts2: sentinel argument for matching function signature
+      standardization.
+    thr: distance ratio check threshold.
+
+  Returns:
+    number of found bidirectional correspondences. If thr is not
+      None, number of bidirectional correspondences that satisfy
+      a distance ratio check.
+  '''
   if len(descs1) == 0 or len(descs2) == 0:
     return 0
 
@@ -12,6 +32,26 @@ def basic(descs1, descs2, pts1=None, pts2=None, thr=None):
 
 
 def spatial(descs1, descs2, pts1, pts2, thr=None):
+  '''
+  Computes the matching score proposed by Pamplona Segundo &
+  Lemes (Pore-based ridge reconstruction for fingerprint
+  recognition, 2015) using bidirectional correspondences
+  between descriptors descs1 and descs2.
+  If thr is provided, correspondences that fail a distance
+  ratio check with threshold thr are discarded.
+
+  Args:
+    descs1: [N, M] array of N descriptors of dimension M each.
+    descs2: [N, M] array of N descriptors of dimension M each.
+    pts1: [N, 2] array of coordinates from which each descriptor
+      of descs1 was computed.
+    pts1: [N, 2] array of coordinates from which each descriptor
+      of descs2 was computed.
+    thr: distance ratio check threshold.
+
+  Returns:
+    matching score between descs1 and descs2.
+  '''
   if len(descs1) == 0 or len(descs2) == 0:
     return 0
 
